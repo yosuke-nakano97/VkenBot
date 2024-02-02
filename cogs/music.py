3758036811153d19271040928aa8149df1ed554d
yosuke-nakano97 <yopike97@gmail.com>
2024-02-02 18:33:58 +0900

    [fix]slashcommandに対応させた

---
+++ cogs/music.py
@@ -0,0 +1,197 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import random
+import datetime
+import asyncio
+
+
+class MusicCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # 音楽流してるとTrueにする
+        self.playing_flag = False
+        # ボイチャに入っていたらTrue
+        self.connected_flag = False
+
+    # joinコマンド
+    @app_commands.command(name="join")
+    async def join(self, intr):
+        print("join")
+        # ボットなら無視
+        if intr.user.bot:
+            await intr.response.send_message("あんたBot", ephemeral = True)
+            return
+        # 送った人がボイチャにいるか
+        if intr.user.voice:
+            channel = intr.user.voice.channel
+            voice_channel = discord.utils.get(self.bot.voice_clients, guild=intr.guild)
+            print(voice_channel)
+
+            if not voice_channel:
+                self.connected_flag = True
+                # ボイスチャンネルに接続していない場合は接続する
+                vc = await channel.connect(timeout=30.0, self_deaf=True)
+                print(f"Connected to {channel}")
+                await intr.response.send_message("やっほー", ephemeral = True)
+            else:
+                # しているとそのまま何も起こらない
+                await intr.response.send_message("もう入ってるじゃん", ephemeral = True)
+        else:
+            # していない場合
+            print("You don't connect to a voice channel")
+            await intr.response.send_message("どこにはいればいいかわからないよ", ephemeral = True)
+
+    # # leaveコマンド
+    @app_commands.command(name="leave")
+    async def leave(self, intr):
+            print("leave")
+            # ボットなら無視
+            if intr.user.bot:
+                await intr.response.send_message("あんたbot", ephemeral = True)
+                return
+            # ボイチャに入ってなかったら
+            if intr.user.voice is None:
+                await intr.response.send_message("ボイチャ入ってないじゃん", ephemeral = True)
+                print("you dont connect to a voice channel")
+                return
+
+            voice_channel = discord.utils.get(self.bot.voice_clients, guild=intr.guild)
+            print(voice_channel)
+            
+            if voice_channel.channel == intr.user.voice.channel:
+                
+                # ボイスチャンネルにしてたら切る
+                await voice_channel.disconnect()
+                self.connected_flag = False
+
+                print(f"Disconnected from {voice_channel}")
+                await intr.response.send_message("ばいばいー", ephemeral = True)
+            else:
+                # ボイスチャンネルに入ってなかったら無視
+                print("You don't connect to a voice channel.")
+                await intr.response.send_message("あんたこのボイチャ入ってないじゃん", ephemeral = True)
+    # playコマンド
+    @app_commands.command(name="play")
+    async def play(self, intr):
+        print("play")
+        # ボットなら無視
+        if intr.user.bot:
+            return
+            await intr.response.send_message("あんたbot", ephemeral = True)
+        # ユーザーがボイチャにいなければ無視
+        if intr.user.voice is None:
+            await intr.response.send_message("ボイチャ入ってないじゃん", ephemeral = True)
+            print("you dont connect to a voice channel")
+            return
+
+        voice_channel = discord.utils.get(self.bot.voice_clients, guild=intr.guild)
+        # ボイチャに入っていなければ無視
+        if voice_channel is None or not voice_channel.is_connected():
+            await intr.response.send_message("ボイチャ入ってないじゃん", ephemeral = True)
+            return
+
+        #   playig(２回以上してたらTrue)で１回目じゃないと無視 
+        if self.playing_flag:
+            await intr.response.send_message("もう流してるよ", ephemeral = True)
+            return
+
+        try:
+            # 1度入ると記憶
+            self.playing_flag = True
+
+            # インタラクションの処理
+            await intr.response.send_message("OK！", ephemeral = True)
+
+            # 音を出す所に入る
+            await self.play_music(voice_channel)
+
+        except discord.errors.ClientException as e:
+            print(f"Error in play command: {e}")
+        finally:
+            # メソッドが終わるとFalseに
+            self.playing_flag = False
+            print("fin")
+                        
+
+    async def play_music(self, voice_channel):
+        # ファイル群の名前をもらってくる
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        target_directory = os.path.join(current_directory, "..", "musics")
+        file_list = os.listdir(target_directory)
+        # エンドレスに1つづ流す
+        while voice_channel.is_connected():
+            filename = random.choice(file_list)
+            # ファイルのパス生成
+            file_path = os.path.join(target_directory, filename)
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            print(file_path)
+            # 音声流す
+            voice_channel.play(discord.FFmpegPCMAudio(file_path))
+            # 前の音声が流れ終わるのをまつ
+            while voice_channel.is_playing():
+                await asyncio.sleep(2)
+
+                # ボイスチャンネルの状態を取得
+                channel_state = discord.utils.get(self.bot.voice_clients, guild=voice_channel.guild)
+
+                # もしボイスチャンネルが変更されたらループを抜ける
+                if channel_state and channel_state.channel.id != voice_channel.channel.id:
+                    voice_channel.stop()
+                    return
+            print("playend")
+
+    async def replay(self, channel):
+        #   playig(２回以上してたらTrue)で１回目じゃないと無視 
+        if self.playing_flag:
+            return
+
+        try:
+            # 1度入ると記憶
+            self.playing_flag = True
+            # 音を出す所に入る
+            await self.play_music(channel)
+        except discord.errors.ClientException as e:
+            print(f"Error in play command: {e}")
+        finally:
+            # メソッドが終わるとFalseに
+            self.playing_flag = False 
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        print(f"on_voice_state_update:member:{member},before{before},after:{after}")
+
+        if member.name != self.bot.user.name:
+            print("no")
+            return
+        
+        if not before.channel:
+            print("first")
+            return
+
+        if not self.connected_flag:
+            print("flag_off!")
+            return
+
+        print("yes")
+        await asyncio.sleep(40)
+
+        previous_channel = before.channel
+        voice_client = discord.utils.get(self.bot.voice_clients, channel=previous_channel)
+        i =1
+        print(voice_client)
+        while (voice_client is None) and i < 4:
+            # ボイスチャンネルに接続していない場合は接続する
+            await asyncio.sleep(40)
+            voice_client = await previous_channel.connect(timeout=30.0, self_deaf=True)
+            print("reConnecting")
+            i+=1
+        
+        if voice_client:
+            print("Connected")
+            if self.playing_flag:
+                await self.replay(voice_client)
+
+async def setup(bot: commands.Bot): 
+        await bot.add_cog(MusicCog(bot))
--- vken.py
+++ vken.py
@@ -1,208 +1,52 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import os
 import asyncio
-import random
-import datetime
-import ast
 from dotenv import load_dotenv
 
-
+#インテント作成
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = discord.ext.commands.Bot(command_prefix="!", intents=intents)
+# Cogのリスト
+INITIAL_EXTENSIONS = [
+  'cogs.music'
+]
 
+# .envから各種変数読み出し    
+load_dotenv()
+
+# DiscordBotを作るためのクラス
+class DiscordBot(commands.Bot):
+    def __init__(self, command_prefix: str, intents: discord.Intents):
+        super().__init__(
+                    intents=intents,
+                    command_prefix=command_prefix
+                    )
+    
+    async def setup_hook(self):
+        SERVERID = os.environ.get("SERVERID")
+        self.tree.copy_global_to(guild=discord.Object(id=SERVERID))
+        await self.tree.sync(guild=discord.Object(id=SERVERID))
+        return await super().setup_hook()
+
+# Botオブジェクト生成
+bot = DiscordBot(intents=intents, command_prefix="!")
+
+# Botが立ち上がったことをお知らせ
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    # MusicCogを追加
-    await bot.add_cog(MusicCog(bot))
-    await bot.add_cog(CoinTrackCog(bot))
 
-
-class MusicCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        # 音楽流してるとTrueにする
-        self.playing = False
-        self.inttraped = False
-        self.flag = False
-
-    # joinコマンド
-    @commands.command()
-    async def join(self, ctx):
-        print("join")
-        # ボットなら無視
-        if ctx.author.bot:
-            return
-        # 送った人がボイチャにいるか
-        if ctx.author.voice:
-            channel = ctx.author.voice.channel
-            voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-            print(voice_channel)
-
-            if not voice_channel:
-                self.flag = False
-                # ボイスチャンネルに接続していない場合は接続する
-                vc = await channel.connect(timeout=30.0, self_deaf=True)
-                print(f"Connected to {channel}")
-            else:
-                # しているとそのまま何も起こらない
-                print("Already connected to a voice channel.")
-        else:
-            # していない場合
-            print("You don't connect to a voice channel")
-
-    # # leaveコマンド
-    @commands.command()
-    async def leave(self, ctx):
-        print("leave")
-        # ボットなら無視
-        if ctx.author.bot:
-            return
-        # ボイチャに入ってなかったら
-        if ctx.author.voice is None:
-            print("you dont connect to a voice channel")
-            return
-
-        voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        print(voice_channel)
-        
-        if voice_channel:
-            
-            # ボイスチャンネルにしてたら切る
-            await voice_channel.disconnect()
-            await self.toggle_reconnect()
-
-            print(f"Disconnected from {voice_channel}")
-        else:
-            # ボイスチャンネルに入ってなかったら無視
-            print("You don't connect to a voice channel.")
-
-    # playコマンド
-    @commands.command()
-    async def play(self, ctx):
-        print("play")
-        # ボットなら無視
-        if ctx.author.bot:
-            return
-        # ユーザーがボイチャにいなければ無視
-        if ctx.author.voice is None:
-            print("you dont connect to a voice channel")
-            return
-
-        voice_channel = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        # ボイチャに入っていなければ無視
-        if voice_channel is None or not voice_channel.is_connected():
-            return
-
-        #   playig(２回以上してたらTrue)で１回目じゃないと無視 
-        if self.playing:
-            return
-
-        try:
-            await self.toggle_reconnect()
-            # 1度入ると記憶
-            self.playing = True
-            # 音を出す所に入る
-            await self.play_music(voice_channel)
-
-        except discord.errors.ClientException as e:
-            print(f"Error in play command: {e}")
-        finally:
-            # メソッドが終わるとFalseに
-            self.playing = False
-            print("fin")
-
-    async def play_music(self, voice_channel):
-        # ファイル群の名前をもらってくる
-        current_directory = os.path.dirname(os.path.realpath(__file__))
-        target_directory = os.path.join(current_directory, "musics")
-        file_list = os.listdir(target_directory)
-        # エンドレスに1つづ流す
-        while voice_channel.is_connected():
-            filename = random.choice(file_list)
-            # ファイルのパス生成
-            file_path = os.path.join(target_directory, filename)
-            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            print(file_path)
-            # 音声流す
-            voice_channel.play(discord.FFmpegPCMAudio(file_path))
-            # 前の音声が流れ終わるのをまつ
-            while voice_channel.is_playing():
-                await asyncio.sleep(2)
-
-                # ボイスチャンネルの状態を取得
-                channel_state = discord.utils.get(bot.voice_clients, guild=voice_channel.guild)
-
-                # もしボイスチャンネルが変更されたらループを抜ける
-                if channel_state and channel_state.channel.id != voice_channel.channel.id:
-                    voice_channel.stop()
-                    return
-            print("playend")
-
-    async def replay(self, channel):
-        #   playig(２回以上してたらTrue)で１回目じゃないと無視 
-        if self.playing:
-            return
-
-        try:
-            # 1度入ると記憶
-            self.playing = True
-            # 音を出す所に入る
-            await self.play_music(channel)
-        except discord.errors.ClientException as e:
-            print(f"Error in play command: {e}")
-        finally:
-            # メソッドが終わるとFalseに
-            self.playing = False 
-
-    async def toggle_reconnect(self):
-        self.flag = not self.flag
-        print(f"flagchange{not self.flag} to {self.flag}")
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        print(f"on_voice_state_update:member:{member},before{before},after:{after}")
-
-        self.playing = False
-        if member.name != bot.user.name:
-            print("no")
-            return
-        
-        if not before.channel:
-            print("first")
-            return
-
-        if not self.flag:
-            print("flag_off!")
-            return
-
-        print("yes")
-        await asyncio.sleep(40)
-
-        previous_channel = before.channel
-        voice_client = discord.utils.get(bot.voice_clients, channel=previous_channel)
-        i =1
-        print(voice_client)
-        while (voice_client is None) and i < 4:
-            # ボイスチャンネルに接続していない場合は接続する
-            await asyncio.sleep(40)
-            voice_client = await previous_channel.connect(timeout=30.0, self_deaf=True)
-            print("reConnecting")
-            i+=1
-        
-        
-        if voice_client:
-            print("Connected")
-            await self.replay(voice_client)
-
-
-
-        
-load_dotenv()
-BOTTOKEN = os.environ.get("BOTTOKEN")
-
-bot.run(BOTTOKEN)
+# ボット立ち上げ
+if __name__ == "__main__":
+    async def boot():
+        # CogをBotに登録
+        for cogs in INITIAL_EXTENSIONS:
+            await bot.load_extension(cogs)
+    asyncio.run(boot())
+    BOTTOKEN = os.environ.get("BOTTOKEN")
+    
+    bot.run(BOTTOKEN)
